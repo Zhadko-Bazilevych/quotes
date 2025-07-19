402655345c3c999db5e36158b0d234e7b57ce009
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-07-19 20:09:07 +0300

    feat: implement twMerge

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,13 +1,17 @@
 import type { ButtonHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
-  const { children, ...rest } = props;
+  const { children, className, ...rest } = props;
 
   return (
     <button
-      className="border rounded border-gray-300 p-1 cursor-pointer hover:bg-neutral-900"
+      className={twMerge(
+        "border rounded border-gray-300 p-1 cursor-pointer hover:bg-neutral-900",
+        className
+      )}
       {...rest}
     >
       {children}
