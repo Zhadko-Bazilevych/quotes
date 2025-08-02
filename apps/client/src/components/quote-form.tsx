b97ec97381a1d7feab3986adf4649a216ff7d161
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-02 19:21:54 +0300

    Feature/use form hook (#3)
    * feat: add useForm hook
    
    * feat: add submission and validation
    
    * refactor: fix validation and submit issues
    
    * feat: use react hook form instead of custom hook

--- apps/client/src/components/quote-form.tsx
+++ apps/client/src/components/quote-form.tsx
@@ -1,5 +1,7 @@
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { useForm } from "react-hook-form";
 
 type QuoteFormProps = {
   id: number;
@@ -10,20 +12,51 @@ type QuoteFormProps = {
   onEdit: () => void;
 };
 
+type QuoteFormState = {
+  author: string;
+  sender: string;
+  content: string;
+  context: string;
+};
+
 export function QuoteForm(props: QuoteFormProps) {
-  const { author, content, context, sender } = props;
+  const { author, content, context, sender, onEdit: toggleEdit } = props;
+  const {
+    formState: { isValid, errors },
+    register,
+    handleSubmit,
+  } = useForm<QuoteFormState>({
+    values: { author, content, context, sender },
+  });
+  const onSubmit = (data: QuoteFormState) => {
+    alert(JSON.stringify(data));
+    toggleEdit();
+  };
 
   return (
-    <form className="flex flex-col gap-3 border rounded border-gray-300 p-2">
+    <form
+      className="flex flex-col gap-3 border rounded border-gray-300 p-2"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="flex justify-between">
-        <Input value={author} />
+        <Input {...register("author", { required: true })} />
       </div>
       <div className="flex justify-between">
-        <Textarea value={content} />
+        <Textarea {...register("content", { required: true })} />
       </div>
       <div>
-        <Textarea value={context} />
-        <Input value={sender} />
+        <Textarea {...register("context", { required: true })} />
+        <div className="flex justify-between">
+          <Input {...register("sender", { required: true })} />
+          <div className="flex gap-2">
+            <Button type="button" onClick={toggleEdit}>
+              Cancel
+            </Button>
+            <Button disabled={!isValid} type="submit">
+              Submit
+            </Button>
+          </div>
+        </div>
       </div>
     </form>
   );
