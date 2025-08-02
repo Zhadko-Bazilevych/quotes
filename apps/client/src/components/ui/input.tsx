529c06453b57812fcc44a6743dcda04bc3c2a15d
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-02 20:46:25 +0300

    feat: remove cursor-pointer and hover styles from form elements

--- apps/client/src/components/ui/input.tsx
+++ apps/client/src/components/ui/input.tsx
@@ -5,7 +5,7 @@ type InputProps = InputHTMLAttributes<HTMLInputElement>;
 export function Input(props: InputProps) {
   return (
     <input
-      className="border rounded border-gray-300 p-1 cursor-pointer hover:bg-neutral-900"
+      className="border rounded border-gray-300 p-1"
       {...props}
     />
   );
--- apps/client/src/components/ui/textarea.tsx
+++ apps/client/src/components/ui/textarea.tsx
@@ -5,7 +5,7 @@ type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
 export function Textarea(props: TextareaProps) {
   return (
     <textarea
-      className="border rounded border-gray-300 p-1 cursor-pointer hover:bg-neutral-900 w-full"
+      className="border rounded border-gray-300 p-1 flex-1"
       {...props}
     />
   );
