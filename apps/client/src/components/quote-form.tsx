6c305f9fb9a45081996c421ba1de18e8bb27d78d
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-02 19:17:22 +0300

    feat: use react hook form instead of custom hook

--- apps/client/src/components/quote-form.tsx
+++ apps/client/src/components/quote-form.tsx
@@ -1,7 +1,7 @@
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
-import { useForm } from "./hooks/use-form";
 import { Button } from "./ui/button";
+import { useForm } from "react-hook-form";
 
 type QuoteFormProps = {
   id: number;
@@ -21,10 +21,14 @@ type QuoteFormState = {
 
 export function QuoteForm(props: QuoteFormProps) {
   const { author, content, context, sender, onEdit: toggleEdit } = props;
-  const { isValid, register, handleSubmit } = useForm<QuoteFormState>({
-    initialValues: { author, content, context, sender },
+  const {
+    formState: { isValid, errors },
+    register,
+    handleSubmit,
+  } = useForm<QuoteFormState>({
+    values: { author, content, context, sender },
   });
-  const submit = (data: Partial<QuoteFormState>) => {
+  const onSubmit = (data: QuoteFormState) => {
     alert(JSON.stringify(data));
     toggleEdit();
   };
@@ -32,7 +36,7 @@ export function QuoteForm(props: QuoteFormProps) {
   return (
     <form
       className="flex flex-col gap-3 border rounded border-gray-300 p-2"
-      onSubmit={(e) => handleSubmit(submit, e)}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <div className="flex justify-between">
         <Input {...register("author", { required: true })} />
@@ -48,7 +52,9 @@ export function QuoteForm(props: QuoteFormProps) {
             <Button type="button" onClick={toggleEdit}>
               Cancel
             </Button>
-            <Button disabled={!isValid} type="submit">Submit</Button>
+            <Button disabled={!isValid} type="submit">
+              Submit
+            </Button>
           </div>
         </div>
       </div>
