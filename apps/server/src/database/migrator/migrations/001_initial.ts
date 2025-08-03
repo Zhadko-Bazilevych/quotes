0109f92eda7015282b9640b2c9a8bc613373eca7
DmytroLeonov <leonovdmitriy02@gmail.com>
2025-08-03 21:52:04 +0300

    feat(db): initial migration

---
+++ apps/server/src/database/migrator/migrations/001_initial.ts
@@ -0,0 +1,22 @@
+import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('quote')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    .addColumn('user', 'varchar(30)', (col) => col.notNull())
+    .addColumn('author', 'varchar(30)', (col) => col.notNull())
+    .addColumn('content', 'varchar(500)', (col) => col.notNull())
+    .addColumn('context', 'varchar(500)', (col) => col.notNull())
+    .addColumn('created_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .addColumn('updated_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull(),
+    )
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('quote').execute();
+}
