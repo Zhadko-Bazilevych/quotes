34d146d74a1e4509b483c8bbc0a65cd760bfe28a
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-05 20:50:10 +0300

    feat: remove test endpoint

--- apps/server/src/quotes/quotes.service.ts
+++ apps/server/src/quotes/quotes.service.ts
@@ -1,19 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { KyselyService } from 'src/database/kysely.service';
-import { CompiledQuery } from 'kysely';
 import { CreateQuoteDto, Quote } from 'src/quotes/quotes.types';
 
 @Injectable()
 export class QuotesService {
   constructor(private readonly db: KyselyService) {}
-
-  async test() {
-    const { rows } = await this.db.executeQuery<{ test: 1 }>(
-      CompiledQuery.raw('select 1 as test', []),
-    );
-
-    return rows[0];
-  }
 
   async getQuoteById(id: number): Promise<Quote> {
     return await this.db
