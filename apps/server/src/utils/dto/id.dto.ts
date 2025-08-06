7888f9b1bcd546987de84a710c222fbc7a424899
DmytroLeonov <leonovdmitriy02@gmail.com>
2025-08-06 23:09:26 +0300

    feat(validation): improve id param validation and error messages

--- apps/server/src/utils/dto/id.dto.ts
+++ apps/server/src/utils/dto/id.dto.ts
@@ -2,7 +2,11 @@ import z from 'zod';
 
 export const idSchema = z
   .object({
-    id: z.coerce.number().int().positive(),
+    id: z.coerce
+      .number('should be a number')
+      .max(2147483647, { abort: true, error: 'should be <= 2147483647' })
+      .min(1, { abort: true, error: 'should be >= 1' })
+      .int('should be an integer'),
   })
   .strict();
 
