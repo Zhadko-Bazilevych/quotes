a933b1ad95bf37b5f169eb784c4c575489e2ef35
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-10 20:37:28 +0300

    feat: render multiple error messages for a single field & assotiate corresponding labels

--- apps/client/src/components/form-item.tsx
+++ apps/client/src/components/form-item.tsx
@@ -1,38 +1,49 @@
+import { useId } from 'react';
 import {
   useFormContext,
-  type FieldError,
   type FieldValues,
   type Path,
   type RegisterOptions,
   type UseFormRegisterReturn,
 } from 'react-hook-form';
 
+type FormElementProps = UseFormRegisterReturn<string> & {
+  id: string;
+};
+
 type FormItemProps<T extends FieldValues> = {
   name: Path<T>;
   label: string;
   rules: RegisterOptions<T, Path<T>>;
-  render: (props: UseFormRegisterReturn<string>) => React.ReactNode;
+  render: (props: FormElementProps) => React.ReactNode;
 };
 
 export function FormItem<T extends FieldValues>(
   props: FormItemProps<T>,
 ): React.ReactNode {
   const { name, label, rules, render } = props;
+  const id = useId();
+  const uniqueName = `${id}-${name}`;
+
   const {
     register,
     formState: { errors },
   } = useFormContext<T>();
-  const error = errors[name] as FieldError | undefined;
-
+  const errorList = (
+    Object.entries(errors[name]?.types ?? {}) as [string, string][]
+  ).map(([type, message]) => ({ type, message }));
   return (
     <div className="flex flex-col">
-      <label>{label}: </label>
-      {render(register(name, rules))}
-      {error?.message ? (
-        <span className="text-red-500">{error.message}</span>
-      ) : (
-        <div className="h-6"></div>
-      )}
+      <label htmlFor={uniqueName}>{label}: </label>
+      {render({ ...register(name, rules), id: uniqueName })}
+      {errorList.map(({ type, message }) => {
+        return (
+          <span key={type} className="text-red-500">
+            {message}
+          </span>
+        );
+      })}
+      {!errorList.length && <div className="h-6"></div>}
     </div>
   );
 }
