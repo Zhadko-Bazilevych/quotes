0ba78abc2b87451c59f9f918f95e7723367ced7f
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-16 20:01:22 +0300

    chore: remove unused icons

--- apps/client/src/components/ui/icons/index.ts
+++ apps/client/src/components/ui/icons/index.ts
@@ -1,7 +1,3 @@
 export { CloseIcon } from './close-icon';
 export { EditIcon } from './edit-icon';
 export { TrashcanIcon } from './trashcan-icon';
-export { DoubleAngleBracketsLeft } from './brackets/double-angle-brackets-left';
-export { DoubleAngleBracketsRight } from './brackets/double-angle-brackets-right';
-export { AngleBracketLeft } from './brackets/angle-bracket-left';
-export { AngleBracketRight } from './brackets/angle-bracket-right';
