8dbc8c7534da28335b262b0f2a9a39fe296e1f79
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-19 00:26:59 +0300

    feat: re-design `QuoteCardSkeleton` by using shadcn

--- apps/client/src/components/quote/skeleton/quote-card-skeleton.tsx
+++ apps/client/src/components/quote/skeleton/quote-card-skeleton.tsx
@@ -1,13 +1,14 @@
+import { Skeleton } from '@/components/ui/skeleton/skeleton';
 import type { JSX } from 'react';
 
 export function QuoteCardSkeleton(): JSX.Element {
   return (
-    <div className="flex flex-col gap-3 rounded animate-pulse p-3">
+    <div className="flex flex-col gap-3 rounded p-3 bg-card">
       <div className="flex justify-between">
-        <div className="w-1/8 h-6 rounded"></div>
-        <div className="w-1/5 h-6 rounded"></div>
+        <Skeleton className="w-1/8 h-5 rounded" />
+        <Skeleton className="w-1/5 h-5 rounded" />
       </div>
-      <div className="w-1/3 h-6 rounded"></div>
+      <Skeleton className="w-1/3 h-5 rounded" />
     </div>
   );
 }
