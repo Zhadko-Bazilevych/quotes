582b884eae0f18e29bc1e74fbf1e26c4d35e1efc
Dmytro Leonov <leonovdmitriy02@gmail.com>
2025-08-19 23:44:18 +0300

    feat: new pagination style

--- apps/client/src/components/quote/quote-pagination-bar.tsx
+++ apps/client/src/components/quote/quote-pagination-bar.tsx
@@ -24,7 +24,73 @@ type QuotePaginationBarProps = {
   size: number;
 };
 
-const defaultSizeVariants = [5, 10, 20, 30, 50];
+const DEFAULT_SIZE_VARIANTS = [10, 20, 30, 50];
+
+type PaginationConfig = {
+  renderPrev: boolean;
+  renderFirstEllipsis: boolean;
+  pages: number[];
+  renderLastEllipsis: boolean;
+  renderNext: boolean;
+};
+
+type PaginationConfigInput = {
+  page: number;
+  totalPages: number;
+};
+
+const MAX_PAGES_TO_RENDER = 5;
+
+function range(start: number, stop?: number): number[] {
+  stop ??= start * 2;
+  const add = stop ? start : 0;
+  return Array.from({ length: stop - start }).map((_, i) => i + add);
+}
+
+function getPaginationConfig({
+  page,
+  totalPages,
+}: PaginationConfigInput): PaginationConfig {
+  let pages = [];
+
+  // TODO: change to left right and generate array at the end of the function
+  if (page < MAX_PAGES_TO_RENDER) {
+    pages = range(1, Math.min(totalPages, MAX_PAGES_TO_RENDER) + 1);
+  } else if (page > totalPages - MAX_PAGES_TO_RENDER) {
+    pages = range(
+      Math.max(1, totalPages - MAX_PAGES_TO_RENDER),
+      totalPages + 1,
+    );
+  } else {
+    pages = range(
+      Math.max(1, page - Math.floor(MAX_PAGES_TO_RENDER / 2)),
+      Math.min(totalPages, page + Math.floor(MAX_PAGES_TO_RENDER / 2)) + 1,
+    );
+  }
+
+  const firstPage = pages[0];
+  const lastPage = pages[pages.length - 1];
+  const renderFirstEllipsis = firstPage > 2;
+  const renderLastEllipsis = lastPage < totalPages - 2;
+
+  if (firstPage === 1) {
+    pages.shift();
+  }
+  if (lastPage === totalPages) {
+    pages.pop();
+  }
+
+  const renderPrev = page > 1;
+  const renderNext = page < totalPages;
+
+  return {
+    renderPrev,
+    renderFirstEllipsis,
+    pages,
+    renderLastEllipsis,
+    renderNext,
+  };
+}
 
 export function QuotePaginationBar(
   props: QuotePaginationBarProps,
@@ -34,16 +100,20 @@ export function QuotePaginationBar(
   const { page, total, size } = props;
   const totalPages = Math.ceil(total / size);
 
-  const leftLimit = Math.max(1, page - 2);
-  const rightLimit = Math.min(totalPages, page + 2);
-  const length = rightLimit - leftLimit + 1;
+  const {
+    renderPrev,
+    renderFirstEllipsis,
+    pages,
+    renderLastEllipsis,
+    renderNext,
+  } = getPaginationConfig({ page, totalPages });
 
-  const isSizeDefault = defaultSizeVariants.includes(size);
+  const isSizeDefault = DEFAULT_SIZE_VARIANTS.includes(size);
 
   return (
-    <Pagination className="gap-1">
+    <Pagination className="justify-end gap-1">
       <PaginationContent>
-        {page > 1 && (
+        {renderPrev && (
           <PaginationItem>
             <PaginationPrevious
               to={quoteListRoute.to}
@@ -51,49 +121,48 @@ export function QuotePaginationBar(
             />
           </PaginationItem>
         )}
-        {page > 3 && (
+        <PaginationItem>
+          <PaginationLink
+            to={quoteListRoute.to}
+            search={{ page: 1, size }}
+            isActive={page === 1}
+          >
+            1
+          </PaginationLink>
+        </PaginationItem>
+        {renderFirstEllipsis && (
           <PaginationItem>
-            <PaginationLink to={quoteListRoute.to} search={{ page: 1, size }}>
-              1
+            <PaginationEllipsis />
+          </PaginationItem>
+        )}
+        {pages.map((currentPage) => (
+          <PaginationItem key={currentPage}>
+            <PaginationLink
+              to={quoteListRoute.to}
+              search={{ page: currentPage, size }}
+              isActive={currentPage === page}
+            >
+              {currentPage}
             </PaginationLink>
           </PaginationItem>
-        )}
-        {page > 4 && (
+        ))}
+        {renderLastEllipsis && (
           <PaginationItem>
             <PaginationEllipsis />
           </PaginationItem>
         )}
-        {Array.from({ length }).map((_, arrayIdx) => {
-          const pageNumber = arrayIdx + leftLimit;
-
-          return (
-            <PaginationItem key={arrayIdx}>
-              <PaginationLink
-                to={quoteListRoute.to}
-                search={{ page: pageNumber, size }}
-                isActive={pageNumber === page}
-              >
-                {pageNumber}
-              </PaginationLink>
-            </PaginationItem>
-          );
-        })}
-        {page < totalPages - 3 && (
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-        )}
-        {page < totalPages - 2 && (
+        {totalPages && totalPages !== 1 && (
           <PaginationItem>
             <PaginationLink
               to={quoteListRoute.to}
               search={{ page: totalPages, size }}
+              isActive={page === totalPages}
             >
               {totalPages}
             </PaginationLink>
           </PaginationItem>
         )}
-        {page < totalPages && (
+        {renderNext && (
           <PaginationItem>
             <PaginationNext
               to={quoteListRoute.to}
@@ -119,7 +188,7 @@ export function QuotePaginationBar(
           <SelectValue placeholder={`${size} / page`} />
         </SelectTrigger>
         <SelectContent>
-          {defaultSizeVariants.map((variant, idx) => (
+          {DEFAULT_SIZE_VARIANTS.map((variant, idx) => (
             <SelectItem key={idx} value={String(variant)}>
               {`${variant} / page`}
             </SelectItem>
