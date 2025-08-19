6149629b45ec062859f4f0b50e95c988b5c4957e
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-19 20:57:18 +0300

    refactor: replace `twMerge` with `cn`

--- apps/client/src/components/ui/input.tsx
+++ apps/client/src/components/ui/input.tsx
@@ -1,10 +1,8 @@
+import { cn } from '@/lib/utils';
 import type { InputHTMLAttributes, JSX } from 'react';
-import { twMerge } from 'tailwind-merge';
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 export function Input({ className, ...rest }: InputProps): JSX.Element {
-  return (
-    <input className={twMerge('border rounded p-1', className)} {...rest} />
-  );
+  return <input className={cn('border rounded p-1', className)} {...rest} />;
 }
