7dc230855202bec48f0e1a0a0921ba3c11dd77c9
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-21 14:50:30 +0300

    refactor: remove new unnecesaary `Input` component

--- apps/client/src/components/ui/old-input.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@/lib/utils';
-import type { InputHTMLAttributes, JSX } from 'react';
-
-type InputProps = InputHTMLAttributes<HTMLInputElement>;
-
-export function Input({ className, ...rest }: InputProps): JSX.Element {
-  return <input className={cn('border rounded p-1', className)} {...rest} />;
-}
