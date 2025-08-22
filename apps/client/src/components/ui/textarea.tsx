f714dae67c73d38758f36238fccafcd4d7658926
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-22 19:50:10 +0300

    style: use shadcn `Textarea`

--- apps/client/src/components/ui/textarea.tsx
+++ apps/client/src/components/ui/textarea.tsx
@@ -1,8 +1,22 @@
+import * as React from 'react';
+
 import { cn } from '@/lib/utils';
-import type { JSX, TextareaHTMLAttributes } from 'react';
+import type { JSX } from 'react';
 
-type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
-
-export function Textarea({ className, ...rest }: TextareaProps): JSX.Element {
-  return <textarea className={cn('border rounded p-1', className)} {...rest} />;
+function Textarea({
+  className,
+  ...props
+}: React.ComponentProps<'textarea'>): JSX.Element {
+  return (
+    <textarea
+      data-slot="textarea"
+      className={cn(
+        'border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+        className,
+      )}
+      {...props}
+    />
+  );
 }
+
+export { Textarea };
