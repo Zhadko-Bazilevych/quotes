a5f17f376d1e09c2ad4f2463609ab71491860e5a
Zhadko Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-08-25 20:09:51 +0300

    feat: add toast on quote delete (#46)
    * build: add shadcn toaster
    
    * feat: add simple toast
    
    * feat: customise sonner toasts
    
    * build: remove unused theme autopicker

---
+++ apps/client/src/components/ui/sonner.tsx
@@ -0,0 +1,26 @@
+import type { JSX } from 'react';
+import { Toaster as Sonner, type ToasterProps } from 'sonner';
+
+const Toaster = ({ ...props }: ToasterProps): JSX.Element => {
+  return (
+    <Sonner
+      className="toaster group"
+      position="top-right"
+      closeButton
+      toastOptions={{
+        unstyled: true,
+        classNames: {
+          toast:
+            'flex items-center gap-3 w-100  bg-background text-popover-foreground border border-border rounded p-3',
+          closeButton:
+            'absolute right-0 top-0 translate-x-1/3 -translate-y-1/3 bg-background border border-border rounded-full size-5 flex justify-center items-center',
+          title: 'text-sm font-bold',
+          description: 'text-sm',
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from '@/routes/route-tree';
+import { Toaster } from '@/components/ui/sonner';
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ if (!rootElement) {
 createRoot(rootElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>,
