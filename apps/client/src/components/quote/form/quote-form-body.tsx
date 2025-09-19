01a01d26e3cb1c87fd593848ab87d7759bfba3e1
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-09-19 20:49:29 +0300

    chore: remove built-in rules for validation

--- apps/client/src/components/quote/form/quote-form-body.tsx
+++ apps/client/src/components/quote/form/quote-form-body.tsx
@@ -13,61 +13,21 @@ export function QuoteFormBody(): JSX.Element {
         name="author"
         label="Author"
         render={(props) => <Input {...props} />}
-        rules={{
-          required: 'Field is required',
-          maxLength: {
-            value: 30,
-            message: 'Author name is too long. Maximum characters <= 30',
-          },
-          minLength: {
-            value: 3,
-            message: 'Author name is too short. Minimum characters >= 3',
-          },
-        }}
       />
       <FormItem<QuoteFormData>
         name="content"
         label="Content"
         render={(props) => <Textarea {...props} />}
-        rules={{
-          required: 'Field is required',
-          maxLength: {
-            value: 500,
-            message: 'Content is too long. Maximum characters <= 500',
-          },
-          minLength: {
-            value: 3,
-            message: 'Content name is too short. Minimum characters >= 3',
-          },
-        }}
       />
       <FormItem<QuoteFormData>
         name="context"
         label="Context"
         render={(props) => <Textarea {...props} />}
-        rules={{
-          required: 'Field is required',
-          maxLength: {
-            value: 500,
-            message: 'Context is too long. Maximum characters <= 500',
-          },
-        }}
       />
       <FormItem<QuoteFormData>
         name="user"
         label="User"
         render={(props) => <Input {...props} />}
-        rules={{
-          required: 'Field is required',
-          maxLength: {
-            value: 30,
-            message: 'User name is too long. Maximum characters <= 30',
-          },
-          minLength: {
-            value: 3,
-            message: 'User name is too short. Minimum characters >= 3',
-          },
-        }}
       />
     </>
   );
