6ff032416935593360569c72d90f26458f817b03
Dmytro Leonov <leonovdmitriy02@gmail.com>
2025-09-22 19:34:41 +0300

    fix: remove unnecessary search query module from app module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,7 +3,6 @@ import { KyselyModule } from 'src/database/kysely.module';
 import { MigratorModule } from 'src/database/migrator/migrator.module';
 import { QuoteModule } from './quote/qoute.module';
 import { ConfigModule } from '@nestjs/config';
-import { SearchQueryModule } from './search-query/search-query.module';
 import { dbConfig } from './config/config.configuration';
 
 @Module({
@@ -17,7 +16,6 @@ import { dbConfig } from './config/config.configuration';
       cache: true,
     }),
     QuoteModule,
-    SearchQueryModule,
   ],
 })
 export class AppModule {}
