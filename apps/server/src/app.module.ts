7acc513bafb538d437306bdbb812dcb43d63f678
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-10-02 20:26:22 +0300

    chore: add `AuthModule` to app

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,6 +4,9 @@ import { MigratorModule } from 'src/database/migrator/migrator.module';
 import { QuoteModule } from './quote/quote.module';
 import { ConfigModule } from '@nestjs/config';
 import { dbConfig } from './config/config.configuration';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { AuthConfigModule } from 'src/auth/auth.config.module';
+import { AuthFactory } from 'src/auth/auth.provider';
 
 @Module({
   imports: [
@@ -14,6 +17,11 @@ import { dbConfig } from './config/config.configuration';
       load: [dbConfig],
       envFilePath: ['.env'],
       cache: true,
+    }),
+    AuthConfigModule,
+    AuthModule.forRoot({
+      auth: AuthFactory.getAuth(),
+      isGlobal: true,
     }),
     QuoteModule,
   ],
