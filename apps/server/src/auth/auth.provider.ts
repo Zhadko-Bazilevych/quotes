31080fc54c97bbd5eef4520312fbebe968575cdc
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-10-06 20:36:02 +0300

    fix(better-auth): config snake-cased table names

--- apps/server/src/auth/auth.provider.ts
+++ apps/server/src/auth/auth.provider.ts
@@ -19,6 +19,48 @@ export class AuthFactory {
       //   },
       // },
       trustedOrigins: ['http://localhost:5173'],
+      user: {
+        modelName: 'user',
+        fields: {
+          emailVerified: 'email_verified',
+          createdAt: 'created_at',
+          updatedAt: 'updated_at',
+        },
+      },
+      session: {
+        modelName: 'session',
+        fields: {
+          expiresAt: 'expires_at',
+          createdAt: 'created_at',
+          updatedAt: 'updated_at',
+          ipAddress: 'ip_address',
+          userAgent: 'user_agent',
+          userId: 'user_id',
+        },
+      },
+      account: {
+        modelName: 'account',
+        fields: {
+          accountId: 'account_id',
+          providerId: 'provider_id',
+          userId: 'user_id',
+          accessToken: 'access_token',
+          refreshToken: 'refresh_token',
+          idToken: 'id_token',
+          accessTokenExpiresAt: 'access_token_expires_at',
+          refreshTokenExpiresAt: 'refresh_token_expires_at',
+          createdAt: 'created_at',
+          updatedAt: 'updated_at',
+        },
+      },
+      verification: {
+        modelName: 'verification',
+        fields: {
+          expiresAt: 'expires_at',
+          createdAt: 'created_at',
+          updatedAt: 'updated_at',
+        },
+      },
     });
   }
 
