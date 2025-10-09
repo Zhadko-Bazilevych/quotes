08a4fdb8241b0f1fa6ceaec2ec2a069870ec83e4
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-10-09 18:25:58 +0300

    fix(better-auth): inject `PostgresDialect` into `AuthFactory`

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -6,6 +6,7 @@ import { dbConfig } from './config/config.configuration';
 import { AuthModule as NestjsBetterAuthModule } from '@thallesp/nestjs-better-auth';
 import { AuthModule } from 'src/auth/auth.module';
 import { AuthFactory } from 'src/auth/auth.provider';
+import { QuoteModule } from 'src/quote/quote.module';
 
 @Module({
   imports: [
@@ -19,12 +20,15 @@ import { AuthFactory } from 'src/auth/auth.provider';
     }),
     AuthModule,
     NestjsBetterAuthModule.forRootAsync({
+      imports: [AuthModule],
       inject: [AuthFactory],
       useFactory: (factory: AuthFactory) => ({
         auth: factory.client,
         isGlobal: true,
+        disableGlobalAuthGuard: true,
       }),
     }),
+    QuoteModule,
   ],
 })
 export class AppModule {}
