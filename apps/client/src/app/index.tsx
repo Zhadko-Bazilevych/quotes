6d6f196a1bb24662199aa0b248f0abb433ec6364
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-11-23 15:40:31 +0200

    build: install zustand

--- apps/client/src/app/index.tsx
+++ apps/client/src/app/index.tsx
@@ -1,10 +1,12 @@
 import type { JSX } from 'react';
 import { Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { Header } from '@/components/header/header';
 
 export function App(): JSX.Element {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col items-center">
+      <Header />
       <div className="my-2 w-full px-2 sm:w-3/5 sm:min-w-160">
         <Outlet />
         <TanStackRouterDevtools position="bottom-right" />
