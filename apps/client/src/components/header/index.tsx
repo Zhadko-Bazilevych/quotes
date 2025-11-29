702fffa2aac8bfedbc29e8efce719615c8ab80ec
Dmytro Leonov <leonovdmitriy02@gmail.com>
2025-11-30 01:59:49 +0200

    feat: enable i18n based on env variable

--- apps/client/src/components/header/index.tsx
+++ apps/client/src/components/header/index.tsx
@@ -2,7 +2,9 @@ import type { JSX } from 'react';
 
 import AuthModal from '@/components/auth/auth-modal';
 import LogoutButton from '@/components/auth/logout-button';
+import { LanguageSelect } from '@/components/ui/language-select';
 import { Skeleton } from '@/components/ui/skeleton';
+import { env } from '@/env';
 import { useSession } from '@/hooks/use-session';
 
 export function Header(): JSX.Element {
@@ -12,9 +14,10 @@ export function Header(): JSX.Element {
     <header className="bg-card flex w-full items-center justify-between border p-2">
       <p>That One Quotes</p>
       <div className="flex items-center gap-2">
-        <span>{data && `Hello there, ${data.user.name}`}</span>
+        {data && <span>{`Hello there, ${data.user.name}`}</span>}
+        {env.VITE_ENABLE_I18N && <LanguageSelect />}
         {data && <LogoutButton />}
-        {isPending && <Skeleton className="bg-primary h-9 w-20 rounded-md" />}
+        {isPending && <Skeleton className="bg-primary h-9 w-16 rounded-md" />}
         {!data && !isPending && <AuthModal />}
       </div>
     </header>
--- apps/client/src/components/quote/add-quote-section.tsx
+++ apps/client/src/components/quote/add-quote-section.tsx
@@ -3,26 +3,25 @@ import { useTranslation } from 'react-i18next';
 
 import { CreateQuoteForm } from '@/components/quote/form/create-quote-form';
 import { Button } from '@/components/ui/button';
-import { LanguageSelect } from '@/components/ui/language-select';
 
 export function AddQuoteSection(): JSX.Element {
   const { t } = useTranslation();
-  const [isCreatingQuote, setIsCreatingQuote] = useState<boolean>(false);
+  const [isCreatingQuote, setIsCreatingQuote] = useState(false);
+
   const toggleCreate = (): void => {
     setIsCreatingQuote((prev) => !prev);
   };
 
   return (
-    <div className="flex gap-3">
+    <>
       {isCreatingQuote && <CreateQuoteForm onCancel={toggleCreate} />}
       {!isCreatingQuote && (
-        <Button className="grow" onClick={toggleCreate} variant="outline">
+        <Button className="py-2" onClick={toggleCreate} variant="outline">
           {t(($) => $.quote.newQuotePrompt, {
             defaultValue: 'Create new quote?',
           })}
         </Button>
       )}
-      <LanguageSelect />
-    </div>
+    </>
   );
 }
