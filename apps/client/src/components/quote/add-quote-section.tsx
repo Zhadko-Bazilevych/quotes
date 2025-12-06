922b2880e09712b4685eaa7845df2abf6816546b
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-12-06 15:48:06 +0200

    chore: add permissions for quotes

--- apps/client/src/components/quote/add-quote-section.tsx
+++ apps/client/src/components/quote/add-quote-section.tsx
@@ -1,27 +1,44 @@
 import { type JSX, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { toast } from 'sonner';
+
+import { useAbility } from '@casl/react';
 
 import { CreateQuoteForm } from '@/components/quote/form/create-quote-form';
 import { Button } from '@/components/ui/button';
+import { AbilityContext } from '@/lib/casl/permissions';
 
 export function AddQuoteSection(): JSX.Element {
   const { t } = useTranslation();
   const [isCreatingQuote, setIsCreatingQuote] = useState(false);
+  const ability = useAbility(AbilityContext);
 
   const toggleCreate = (): void => {
     setIsCreatingQuote((prev) => !prev);
   };
 
+  const onClick = (): void => {
+    if (ability.can('create', 'Quote')) {
+      toggleCreate();
+    } else {
+      toast.warning(
+        t(($) => $.quote.createQuoteUnauthorized, {
+          defaultValue: 'You need to sign in to create a quote.',
+        }),
+      );
+    }
+  };
+
   return (
     <>
-      {isCreatingQuote && <CreateQuoteForm onCancel={toggleCreate} />}
       {!isCreatingQuote && (
-        <Button className="py-2" onClick={toggleCreate} variant="outline">
+        <Button className="py-2" onClick={onClick} variant="outline">
           {t(($) => $.quote.newQuotePrompt, {
             defaultValue: 'Create new quote?',
           })}
         </Button>
       )}
+      {isCreatingQuote && <CreateQuoteForm onCancel={toggleCreate} />}
     </>
   );
 }
--- apps/client/src/lib/casl/permissions.ts
+++ apps/client/src/lib/casl/permissions.ts
@@ -7,25 +7,28 @@ import {
 } from '@casl/ability';
 import { createContextualCan } from '@casl/react';
 
-import type { AppUser as User } from '@/types/auth';
+import type { AppUser, AppUser as User } from '@/types/auth';
 import type { Quote } from '@/types/quote';
 
 type Actions = 'manage' | 'read' | 'update' | 'delete' | 'create';
-type Subjects = Quote | 'Quote';
+type Subjects = 'all' | Quote | 'Quote' | AppUser | 'User';
 
 type AppAbility = MongoAbility<[Actions, Subjects]>;
 
 export const AbilityContext = createContext<AppAbility>({} as AppAbility);
 export const Can = createContextualCan<AppAbility>(AbilityContext.Consumer);
 
-export function defineAbilityFor(user?: User): AppAbility {
+export function defineAbilityFor(_user?: User): AppAbility {
   const { can, build } = new AbilityBuilder<AppAbility>(createMongoAbility);
 
-  can('read', 'Quote');
+  // can('read', 'Quote');
 
-  if (user) {
-    can('manage', 'Quote', { userId: user.id });
-  }
+  // if (user) {
+  //   can('manage', 'Quote', { userId: user.id });
+  //   can('manage', 'User', { id: user.id });
+  // }
+
+  can('manage', 'all');
 
   return build({
     detectSubjectType: (object) => object.__typename,
