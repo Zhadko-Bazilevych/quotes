0811631cc5ae6dc4a37c7a557b9db0443301a14f
Dmytro Leonov <leonovdmitriy02@gmail.com>
2025-12-13 23:28:51 +0200

    fix: set `sameSite` to `none` to be able to set auth cookies

--- apps/server/src/auth/auth.provider.ts
+++ apps/server/src/auth/auth.provider.ts
@@ -22,6 +22,11 @@ export class AuthFactory {
         database: {
           generateId: false,
         },
+        // TODO: check whether sameSite can remain lax
+        defaultCookieAttributes: {
+          sameSite: 'none',
+          secure: true,
+        },
       },
       baseURL: config.get('auth.betterAuthUrl', { infer: true }),
       trustedOrigins: config.get('app.cors', { infer: true }),
