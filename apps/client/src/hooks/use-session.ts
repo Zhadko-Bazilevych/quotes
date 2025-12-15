e66435b2161730b5d9e0d69f1e6a888cd186fae2
Dmytro Leonov <leonovdmitriy02@gmail.com>
2025-12-15 16:54:21 +0200

    fix: user role types and casl permissions

--- apps/client/src/hooks/use-session.ts
+++ apps/client/src/hooks/use-session.ts
@@ -24,7 +24,6 @@ export const useSession = (): UseSessionReturn => {
       ...sessionUser,
       role,
       id: Number(sessionUser.id),
-      role: sessionUser.role as AppUser['role'],
       __typename: 'User',
     };
   }, [sessionUser]);
