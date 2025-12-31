24a0904d3caad4c9b3a059203ee32eb98e4e062d
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2025-12-31 20:03:25 +0200

    feat(server): add permissions based on roles

--- apps/server/src/auth/permissions.ts
+++ apps/server/src/auth/permissions.ts
@@ -12,10 +12,17 @@ type Subjects = 'all' | Quote | 'Quote' | AppUser | 'User';
 
 export type AppAbility = MongoAbility<[Actions, Subjects]>;
 
-export function defineAbilityFor(_user: AppUser | null): AppAbility {
+export function defineAbilityFor(user: AppUser | null): AppAbility {
   const { can, build } = new AbilityBuilder<AppAbility>(createMongoAbility);
 
-  can('manage', 'all');
+  if (user?.role.includes('admin')) {
+    can('manage', 'all');
+  }
+
+  if (user?.role.includes('user')) {
+    can('manage', 'Quote', { userId: user.id });
+    can('manage', 'User', { id: user.id });
+  }
 
   return build({
     detectSubjectType: (object) => object.__typename,
