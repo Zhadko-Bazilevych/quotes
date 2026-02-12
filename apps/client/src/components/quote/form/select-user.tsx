1f0c27327f633d749c5411a7655ca38c98c38a2b
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2026-02-12 19:36:58 +0200

    chore: implement combobox select to choose user

---
+++ apps/client/src/components/quote/form/select-user.tsx
@@ -0,0 +1,81 @@
+import type { JSX } from 'react';
+import { useMemo, useState } from 'react';
+import type {
+  ControllerRenderProps,
+  FieldPath,
+  FieldValues,
+} from 'react-hook-form';
+
+import { keepPreviousData } from '@tanstack/react-query';
+
+import {
+  Combobox,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList,
+} from '@/components/ui/combobox';
+import { useDebounce } from '@/hooks/use-debounce';
+import { useUserList } from '@/hooks/use-user-list';
+import type { UserSearchItem } from '@/types/user';
+
+type SelectUserProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+  field: ControllerRenderProps<TFieldValues, TName>;
+  defaultValue?: UserSearchItem;
+};
+
+export function SelectUser<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({ field, defaultValue }: SelectUserProps<TFieldValues, TName>): JSX.Element {
+  const [selectedValue, setSelectedValue] = useState<UserSearchItem | null>(
+    defaultValue ?? null,
+  );
+  const [q, setQ] = useState(defaultValue?.name ?? '');
+  const debouncedQ = useDebounce(q, 500);
+  const { data } = useUserList(
+    { q: debouncedQ, limit: 8 },
+    { placeholderData: keepPreviousData },
+  );
+
+  const items = useMemo(() => {
+    if (!selectedValue) {
+      return data;
+    }
+    const noSelectedData =
+      data?.filter((user) => user.id !== selectedValue.id) ?? [];
+
+    return [selectedValue, ...noSelectedData];
+  }, [data, selectedValue]);
+
+  return (
+    <Combobox<UserSearchItem>
+      items={items}
+      itemToStringLabel={(user) => user.name}
+      filter={null}
+      inputValue={q}
+      onInputValueChange={setQ}
+      value={selectedValue ?? null}
+      onValueChange={(user) => {
+        setSelectedValue(user);
+        field.onChange(user?.id ?? undefined);
+      }}
+    >
+      <ComboboxInput placeholder="Select a user" />
+      <ComboboxContent>
+        <ComboboxEmpty>No users found.</ComboboxEmpty>
+        <ComboboxList>
+          {(item: UserSearchItem) => (
+            <ComboboxItem key={item.id} value={item}>
+              {item.name}
+            </ComboboxItem>
+          )}
+        </ComboboxList>
+      </ComboboxContent>
+    </Combobox>
+  );
+}
