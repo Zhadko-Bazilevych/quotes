3f24615dd1b840f36c7a58c057b89fe40cfeb32c
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2026-02-26 18:53:40 +0200

    fix(db): rename `post_visibility` to `quote_visibility`

--- apps/server/src/database/migrator/migrations/1766507457000-add-quote-visibility.ts
+++ apps/server/src/database/migrator/migrations/1766507457000-add-quote-visibility.ts
@@ -5,13 +5,13 @@ export const addQuoteVisibility1766507457000: CustomMigration = {
   name: '1766507457000-add-quote-visibility',
   up: async (db: Kysely<unknown>): Promise<void> => {
     await db.schema
-      .createType('post_visibility')
+      .createType('quote_visibility')
       .asEnum(['public', 'private'])
       .execute();
 
     await db.schema
       .alterTable('quote')
-      .addColumn('visibility', sql`post_visibility`, (col) =>
+      .addColumn('visibility', sql`quote_visibility`, (col) =>
         col.defaultTo(sql`'private'`).notNull(),
       )
       .execute();
@@ -19,6 +19,6 @@ export const addQuoteVisibility1766507457000: CustomMigration = {
 
   down: async (db: Kysely<unknown>): Promise<void> => {
     await db.schema.alterTable('quote').dropColumn('visibility').execute();
-    await db.schema.dropType('post_visibility').execute();
+    await db.schema.dropType('quote_visibility').execute();
   },
 };
