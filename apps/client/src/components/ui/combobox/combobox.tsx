8acafd89af68aec692d6440fa1840c04a6d0eaff
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2026-03-11 17:59:36 +0200

    chore: remove 'use client' line

--- apps/client/src/components/ui/combobox/combobox.tsx
+++ apps/client/src/components/ui/combobox/combobox.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { CheckIcon, ChevronDownIcon, XIcon } from 'lucide-react';
 import type { JSX } from 'react';
 import * as React from 'react';
--- apps/client/src/components/ui/form/form.tsx
+++ apps/client/src/components/ui/form/form.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import type { JSX } from 'react';
 import * as React from 'react';
 import {
