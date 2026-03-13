f1df22633f282e0a5bfe3e943ea960cee7e69211
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2026-03-13 14:53:24 +0200

    chore(update-quote): make zod schema fields required

--- apps/client/src/components/quote/form/update-quote-schema.ts
+++ apps/client/src/components/quote/form/update-quote-schema.ts
@@ -1,11 +1,11 @@
 import z from 'zod';
 
 export const updateQuoteSchema = z.object({
-  author: z.string().min(3).max(30).optional(),
+  author: z.string().min(3).max(30),
   userId: z.number().optional(),
-  content: z.string().min(3).max(500).optional(),
-  context: z.string().max(500).optional(),
-  visibility: z.enum(['public', 'private']).optional(),
+  content: z.string().min(3).max(500),
+  context: z.string().max(500),
+  visibility: z.enum(['public', 'private']),
 });
 
 export type UpdateQuoteSchema = z.infer<typeof updateQuoteSchema>;
