fe039aa601d16a45af93dc10acceff11090db3bc
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2026-06-09 00:56:57 +0300

    chore(quote-card): put edit and delete buttons inside dropdown menu

--- apps/client/src/components/quote/quote-card.tsx
+++ apps/client/src/components/quote/quote-card.tsx
@@ -1,19 +1,25 @@
 import {
-  ChevronDown,
-  ChevronUp,
-  PencilIcon,
+  EllipsisVerticalIcon,
   ThumbsDownIcon,
   ThumbsUpIcon,
 } from 'lucide-react';
 import React, { type JSX, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { useAbility } from '@casl/react';
+
 import { DeleteModal } from '@/components/quote/delete-quote-modal';
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { useDeleteQuoteMutation } from '@/hooks/use-delete-quote';
 import { useDisclosure } from '@/hooks/use-disclosure';
 import { useVoteQuoteMutation } from '@/hooks/use-vote-quote';
-import { Can } from '@/lib/casl/permissions';
+import { AbilityContext, Can } from '@/lib/casl/permissions';
 import type { Quote } from '@/types/quote';
 import { formatDatetime } from '@/utils/formatters';
 
@@ -34,6 +40,7 @@ export const QuoteCard = React.memo(function QuoteCard(
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { t } = useTranslation();
+  const ability = useAbility(AbilityContext);
 
   const toggleDetails = (): void => setIsDetailsOpen((prev) => !prev);
 
@@ -55,12 +62,49 @@ export const QuoteCard = React.memo(function QuoteCard(
     <div className="bg-card flex flex-col gap-3 rounded border p-2">
       <div className="flex justify-between">
         <span className="truncate">{quote.author}</span>
-        <span className="hidden sm:block">
-          Created: {formatDatetime(quote.createdAt)}
-        </span>
+        <div className="flex items-center gap-1">
+          <span className="hidden sm:block">
+            Created: {formatDatetime(quote.createdAt)}
+          </span>
+          {(ability.can('delete', quote) || ability.can('update', quote)) && (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="sm">
+                  <EllipsisVerticalIcon />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-40" align="start">
+                <Can I="update" this={quote}>
+                  <DropdownMenuItem onClick={toggleEdit}>
+                    <span>Edit</span>
+                  </DropdownMenuItem>
+                </Can>
+                <Can I="delete" this={quote}>
+                  <DropdownMenuItem asChild>
+                    <DeleteModal
+                      isOpen={isOpen}
+                      onOpen={onOpen}
+                      onClose={onClose}
+                      onOk={deleteQuote}
+                      isDeleting={isPending}
+                    />
+                  </DropdownMenuItem>
+                </Can>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
       </div>
       <p className="wrap-break-word whitespace-pre-wrap">{quote.content}</p>
       <div className="flex justify-between">
+        <Button
+          className="self-start"
+          onClick={toggleDetails}
+          variant="outline"
+          size="sm"
+        >
+          Details
+        </Button>
         <div className="flex gap-1">
           <Button
             onClick={() => {
@@ -84,30 +128,6 @@ export const QuoteCard = React.memo(function QuoteCard(
           >
             <ThumbsDownIcon /> {quote.dislikes}
           </Button>
-          <Button
-            className="self-start"
-            onClick={toggleDetails}
-            variant="ghost"
-            size="sm"
-          >
-            {isDetailsOpen ? <ChevronUp /> : <ChevronDown />}
-          </Button>
-        </div>
-        <div className="flex items-start gap-1">
-          <Can I="update" this={quote}>
-            <Button onClick={toggleEdit} variant="outline" size="icon">
-              <PencilIcon />
-            </Button>
-          </Can>
-          <Can I="delete" this={quote}>
-            <DeleteModal
-              isOpen={isOpen}
-              onOpen={onOpen}
-              onClose={onClose}
-              onOk={deleteQuote}
-              isDeleting={isPending}
-            />
-          </Can>
         </div>
       </div>
       {isDetailsOpen && (
